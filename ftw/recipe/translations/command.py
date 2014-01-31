d7778ecb939b28747d026ce19ed8e3e432682e3d
Jonas Baumann <jone@jone.ch>
2014-01-31 11:56:00 +0100

    Pass spreadsheet url to the commands.

--- ftw/recipe/translations/command.py
+++ ftw/recipe/translations/command.py
@@ -1,4 +1,5 @@
 from ftw.recipe.translations import sync
+from ftw.recipe.translations import upload
 from pkg_resources import get_distribution
 import argparse
 import sys
@@ -8,7 +9,7 @@ def version():
     return get_distribution('ftw.recipe.translations').version
 
 
-def main(sources_dir, spreadsheet):
+def main(sources_dir, spreadsheet_url):
     parser = argparse.ArgumentParser(sys.argv[0])
     parser.add_argument('--version', action='version',
                         version='%%(prog)s %s' % version())
@@ -20,6 +21,7 @@ def main(sources_dir, spreadsheet):
 
     subparsers = parser.add_subparsers(help='Actions', dest='action')
     sync.setup_argparser(subparsers)
+    upload.setup_argparser(subparsers)
 
     args = parser.parse_args()
-    args.func(args)
+    args.func(args, spreadsheet_url)
