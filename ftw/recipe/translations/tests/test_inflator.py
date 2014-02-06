960ac624bf33eb7f90c6f9685320479dcb8ba415
David Erni <david.erni@4teamwork.ch>
2014-02-06 16:01:15 +0100

    Improve inflator translation extraction tests.

--- ftw/recipe/translations/tests/test_inflator.py
+++ ftw/recipe/translations/tests/test_inflator.py
@@ -12,18 +12,46 @@ class TestRebuildPotCommand(TestCase):
 
     def setUp(self):
         self.tempdir = self.layer[u'tempdir']
+        self.potfile = os.path.join(self.tempdir, 'inflator.pot')
+        self.profiles_dir = os.path.join(self.tempdir, 'foo', 'foo',
+                                         'profiles')
+        self.domain = 'foo.foo'
 
-    def test_extract_messages(self):
+    def test_no_potfile_with_missing_profiles_dir(self):
+        fshelpers.create_structure(self.tempdir, {'foo/foo/__init__.py': ''})
+
+        rebuild_pot(self.potfile, self.profiles_dir, self.domain)
+        self.assertFalse(os.path.exists(self.potfile))
+
+    def test_no_potfile_with_missing_inflator_content(self):
+        fshelpers.create_structure(self.tempdir,
+                        {os.path.join(self.profiles_dir, 'metadata.xml'): ''})
+
+        rebuild_pot(self.potfile, self.profiles_dir, self.domain)
+        self.assertFalse(os.path.exists(self.potfile))
+
+    def test_no_potfile_with_missing_translations(self):
         fshelpers.create_structure(self.tempdir, {
                 'foo/foo/profiles/default/content_creation/content.json':
-                        fshelpers.asset('content.json'),
+                        fshelpers.asset('untranslated_content.json'),
                     })
 
-        rebuild_pot(os.path.join(self.tempdir, 'inflator.pot',),
-                    os.path.join(self.tempdir, 'foo', 'foo', 'profiles'),
-                    'foo.foo')
+        rebuild_pot(self.potfile, self.profiles_dir, self.domain)
 
-        messages = pohelpers.messages(self.tempdir, 'inflator.pot')
+        self.assertFalse(os.path.exists(self.potfile))
+
+    def test_extract_messages(self):
+        potfile = os.path.join(self.tempdir, 'somedir', 'inflator.pot')
+        fshelpers.create_structure(self.tempdir, {
+                'foo/foo/profiles/default/content_creation/content.json':
+                        fshelpers.asset('translated_content.json'),
+                    })
+        self.assertFalse(os.path.exists(os.path.dirname(potfile)))
+
+        rebuild_pot(potfile, self.profiles_dir, self.domain)
+
+        self.assertTrue(os.path.exists(os.path.dirname(potfile)))
+        messages = pohelpers.messages(potfile)
+        self.assertTrue(os.path.exists(potfile))
         self.assertIn('Foo', messages)
-
 
