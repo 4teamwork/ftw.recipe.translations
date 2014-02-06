5dc9225a936c92e6c110fa2f08c21c83ed03f4d8
Jonas Baumann <jone@jone.ch>
2014-02-06 17:12:45 +0100

    Mockup i18nbuild command.

--- ftw/recipe/translations/i18nbuild/command.py
+++ ftw/recipe/translations/i18nbuild/command.py
@@ -2,3 +2,21 @@
 
 def main(package_name, i18n_domain, package_namespace, package_directory):
     pass
+
+
+def build_translations(package_directory, i18n_domain, new_languages=None):
+    rebuild_inflator(package_directory, i18n_domain)
+    rebuild_potfiles(package_directory)
+    sync_potfiles(package_directory, new_languages)
+
+
+def rebuild_inflator(package_directory, i18n_domain):
+    pass
+
+
+def rebuild_potfiles(package_directory, i18n_domain):
+    pass
+
+
+def sync_potfiles(package_directory, new_languages=None):
+    pass
